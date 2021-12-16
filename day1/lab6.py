31cf27a81871689492f021ddbb89203cd667c882
Dev khanal <iamdev.tech@gmail.com>
2021-12-16 08:57:06 +0545

    Labwork

--- day1/lab6.py
+++ day1/lab6.py
@@ -1,9 +1,4 @@
-#even number
-listnum=range(0,11,2)
-sumB=sum(listnum)
-print(sumB)
-
-#odd number
-listnum=range(1,10,2)
-sumA=sum(listnum)
-print(sumA)
+def add(first, second):
+  ans=first + second
+  print('The sum is' ans)
+add(4,6)  
---
+++ day1/rk.py
@@ -0,0 +1,4 @@
+def show(*num):
+    z=num[0]+num[1]+num[2]
+    print(z)
+show(5,4,3)
